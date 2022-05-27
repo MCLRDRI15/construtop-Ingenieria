aecbfc2b4c5345b48524209ac09c4f2d09e3ea6b
Michael Alejandro Rios Rodriguez <71363790+MCLRDRI15@users.noreply.github.com>
2022-05-27 09:44:50 -0500

    especializacion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,13 +6,10 @@ import Image from "next/image";
 import CustomFooter from "src/components/footer/Footer";
 
 import brujula from "src/assets/nosotros/brujula.png";
-import casco from "src/assets/nosotros/casco.png";
-import retroexcavadora from "src/assets/nosotros/retroexcavadora.png";
-import admin from "src/assets/nosotros/admin.png";
-import tecnico from "src/assets/nosotros/tecnico.png";
-import mercadeo from "src/assets/nosotros/mercadeo.png";
-import identifica from "src/assets/identifica/identifica-1.png";
-import identifica2 from "src/assets/identifica/identifica-2.png";
+import estabilidad from "src/assets/index/estabilidad.jpeg";
+import manejoAguas from "src/assets/index/manejoAguas.jpeg";
+import urbanismos from "src/assets/index/urbanismos.jpg";
+import viales from "src/assets/index/viales.jpg";
 
 export default function Inicio() {
   return (
@@ -62,80 +59,114 @@ export default function Inicio() {
                 Nos especializamos en:
               </span>
             </div>
-            <div className="lg:m-10 p-3 md:grid md:grid-col-2 md:grid-rows-2 md:grid-flow-col md:gap-4 mx-4 md:mx-14 lg:mx-44">
-              <div className="border-2 border-dashed bg-gray-100 border-gray-600 hover:border-yellow-500 my-3  mx-auto rounded-2xl lg:w-96">
-                <div className="flex flex-col content-around items-center">
-                  <div className="border-2 border-yellow-500 rounded-full p-2 mx-4 my-4">
+            <div className="p-5">
+              <div className="flex flex-col items-center md:grid md:grid-col-2 md:grid-rows-2 md:grid-flow-col md:gap-4">
+                <div className="relative w-full md:w-80 lg:w-96 h-48 overflow-hidden border-2 bg-gray-100 border-gray-600 hover:border-yellow-500 hover:shadow-xl hover:shadow-gray-500 my-3 md:ml-auto rounded-2xl">
+                  <div className="absolute w-full z-10 opacity-80">
                     <Image
-                      height={60}
-                      width={70}
-                      src={casco}
+                      height={500}
+                      width={800}
+                      src={estabilidad}
                       alt="Obras civiles"
-                      className="shadow shadow-gray-300"
+                      className="object-cover"
                     />
                   </div>
-                  <div className="my-3 text-center mx-2">
-                    <span className="font-serif">
-                      La ejecución de Obras civiles a entidades Públicas y / o
-                      Privadas y personas naturales.
-                    </span>
+                  <div className="absolute w-full flex flex-col content-around items-center z-20">
+                    <div className="border-2 border-black bg-gray-50 rounded-full p-2 mx-4 my-4">
+                      <Image
+                        height={60}
+                        width={70}
+                        src={brujula}
+                        alt="Obras civiles"
+                        className="shadow shadow-gray-300 object-cover"
+                      />
+                    </div>
+                    <div className="my-3 text-center mx-2">
+                      <span className="font-serif text-yellow-500 bg-white rounded-lg p-1 font-medium">
+                        Obras de estabilidad
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="border-2 border-dashed bg-gray-100 border-gray-600 hover:border-yellow-500  my-3  mx-auto rounded-2xl lg:w-96">
-                <div className="flex flex-col content-around items-center">
-                  <div className="border-2 border-yellow-500 rounded-full p-2 mx-4 my-4">
+                <div className="relative w-full md:w-80 lg:w-96 h-48 overflow-hidden border-2 bg-gray-100 border-gray-600 hover:border-yellow-500 hover:shadow-xl hover:shadow-gray-500 my-3  md:ml-auto rounded-2xl">
+                  <div className="absolute w-full z-10 opacity-80">
                     <Image
-                      height={60}
-                      width={70}
-                      src={brujula}
+                      height={500}
+                      width={800}
+                      src={manejoAguas}
                       alt="Obras civiles"
-                      className="shadow shadow-gray-300"
+                      className="object-cover"
                     />
                   </div>
-                  <div className="my-3 text-center mx-2">
-                    <span className="font-serif">
-                      Actividades de arquitectura e ingeniería y actividades
-                      conexas asesoramiento técnico.
-                    </span>
+                  <div className="absolute w-full flex flex-col content-around items-center z-20">
+                    <div className="border-2 border-black bg-gray-50 rounded-full p-2 mx-4 my-4">
+                      <Image
+                        height={60}
+                        width={70}
+                        src={brujula}
+                        alt="Obras civiles"
+                        className="shadow shadow-gray-300 object-cover"
+                      />
+                    </div>
+                    <div className="my-3 text-center mx-2">
+                      <span className="font-serif text-yellow-500 bg-white rounded-lg p-1 font-medium">
+                        Manejo de aguas y de BioIngeniería
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="border-2 border-dashed bg-gray-100 border-gray-600 hover:border-yellow-500  my-3  mx-auto rounded-2xl lg:w-96">
-                <div className="flex flex-col content-around items-center">
-                  <div className="border-2 border-yellow-500 rounded-full p-2 mx-4 my-4">
+                <div className="relative w-full md:w-80 lg:w-96 h-48 overflow-hidden border-2 bg-gray-100 border-gray-600 hover:border-yellow-500 hover:shadow-xl hover:shadow-gray-500  my-3  md:mr-auto rounded-2xl">
+                  <div className="absolute w-full z-10 opacity-80">
                     <Image
-                      height={60}
-                      width={70}
-                      src={brujula}
+                      height={500}
+                      width={800}
+                      src={urbanismos}
                       alt="Obras civiles"
-                      className="shadow shadow-gray-300"
+                      className="object-cover"
                     />
                   </div>
-                  <div className="my-3 text-center mx-2">
-                    <span className="font-serif">
-                      Actividades de arquitectura e ingeniería y actividades
-                      conexas asesoramiento técnico.
-                    </span>
+                  <div className="absolute w-full flex flex-col content-around items-center z-20">
+                    <div className="border-2 border-black bg-gray-50 rounded-full p-2 mx-4 my-4">
+                      <Image
+                        height={60}
+                        width={70}
+                        src={brujula}
+                        alt="Obras civiles"
+                        className="shadow shadow-gray-300 object-cover"
+                      />
+                    </div>
+                    <div className="my-3 text-center mx-2">
+                      <span className="font-serif text-yellow-500 bg-white rounded-lg p-1 font-medium">
+                        Urbanismos
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="border-2 border-dashed bg-gray-100 border-gray-600 hover:border-yellow-500  my-3  mx-auto rounded-2xl lg:w-96">
-                <div className="flex flex-col content-around items-center">
-                  <div className="border-2 border-yellow-500 rounded-full p-2 mx-4 my-4">
+                <div className="relative w-full md:w-80 lg:w-96 h-48 overflow-hidden border-2 bg-gray-100 border-gray-600 hover:border-yellow-500 hover:shadow-xl hover:shadow-gray-500 my-3  md:mr-auto rounded-2xl">
+                  <div className="absolute w-full z-10 opacity-80">
                     <Image
-                      height={60}
-                      width={70}
-                      src={brujula}
+                      height={500}
+                      width={800}
+                      src={viales}
                       alt="Obras civiles"
-                      className="shadow shadow-gray-300"
+                      className="object-cover"
                     />
                   </div>
-                  <div className="my-3 text-center mx-2">
-                    <span className="font-serif">
-                      Actividades de arquitectura e ingeniería y actividades
-                      conexas asesoramiento técnico.
-                    </span>
+                  <div className="absolute w-full flex flex-col content-around items-center z-20">
+                    <div className="border-2 border-black bg-gray-50 rounded-full p-2 mx-4 my-4">
+                      <Image
+                        height={60}
+                        width={70}
+                        src={brujula}
+                        alt="Obras civiles"
+                        className="shadow shadow-gray-300 object-cover"
+                      />
+                    </div>
+                    <div className="my-3 text-center mx-2">
+                      <span className="font-serif text-yellow-500 bg-white rounded-lg p-1 font-medium">
+                        Obras Viales
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
